9b6149d9664514d0777e7cfbb31029f6c7bb3f75
Vieirajrz7 <joaogedavim787@gmail.com>
2024-09-07 11:12:29 -0300

    saving users in the database

--- app_register_user/models.py
+++ app_register_user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Users(models.Model):
+class User(models.Model):
     id_user = models.AutoField(primary_key=True)
     name = models.TextField(max_length=255)
     age = models.IntegerField()
--- app_register_user/views.py
+++ app_register_user/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import User
 
 def home(request):
     return render(request,'users/home.html')
 
 def listUsers(request):
-    pass
+    # Save datas of screen for the database
+    new_user = User() 
+    new_user.name = request.POST.get('name')
+    new_user.age = request.POST.get('age')
+    
+    new_user.save()
+    # Show users registred
+    
+    users = {
+        'users': User.objects.all()
+    }
+    
+    # Return datas for list page for users
+    return render(request,'users/users.html',users)
