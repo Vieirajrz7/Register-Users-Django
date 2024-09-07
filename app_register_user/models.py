83fddee704e70353d95414bc25cf0e913bd422d3
Vieirajrz7 <joaogedavim787@gmail.com>
2024-09-07 10:56:28 -0300

    create SQLlite database

--- app_register_user/models.py
+++ app_register_user/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+class Users(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=255)
+    age = models.IntegerField()
