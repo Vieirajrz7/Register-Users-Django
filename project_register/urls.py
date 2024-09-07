665af44d753eb5791992330353c503ac6d5f7d3c
Vieirajrz7 <joaogedavim787@gmail.com>
2024-09-07 10:53:20 -0300

    initial connection with DataBase

--- project_register/urls.py
+++ project_register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app_register_user import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('users/',views.listUsers,name='list_users')
 ]
